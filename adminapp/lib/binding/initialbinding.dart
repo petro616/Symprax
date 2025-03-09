65bbebf84869842ce0b3ff07f97bc75ea876a1b1
Rebal Daher <rebaldaher29@gmail.com>
2025-03-09 06:23:21 -0700

    Delete adminapp directory

--- adminapp/lib/binding/initialbinding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:AdminApp/core/class/crud.dart';
-import 'package:get/get.dart';
-
-class Initialbindings extends Bindings {
-  @override
-  void dependencies() {
-    Get.put(Crud());
-  }
-}
--- adminapp/lib/core/function/handlingdatacontroller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:AdminApp/core/class/status_request.dart';
-
-handlingdata(response) {
-  if (response is StatusRequest) {
-    return response;
-  } else {
-    return StatusRequest.success;
-  }
-}
